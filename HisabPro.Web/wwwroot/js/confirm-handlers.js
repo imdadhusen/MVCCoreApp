e0523bf355d35297e0f387794c227db8b3bcd84c
Imdadhusen Sunasara <imdadhusen.sunasara@hotmail.com>
2024-09-15 15:19:34 +0530

    UI integration for Delete category

---
+++ HisabPro.Web/wwwroot/js/confirm-handlers.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    // Confirm modal hides when clicking the cancel button or close button (×)
+    $('#confirmModal').find('.btn-close, .btn-secondary').click(function () {
+        $('#confirmModal').modal('hide');
+    });
+});
+
+function showConfirm(confirmLabel, confirmText, yesCallback, yesCallbackData) {
+    $('#confirmModal').modal('show');
+    $('#confirmModalLabel').text(confirmLabel);
+    $('#confirmModalText').text(confirmText);
+    $('#confirmButton').off('click').click(function () {
+        if (yesCallback && typeof yesCallback == "function") {
+            yesCallback(yesCallbackData);
+        }
+    });
+}
