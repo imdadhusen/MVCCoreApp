4844f99a583ed921b7857db9479a222549ff1e4d
Imdadhusen Sunasara <imdadhusen.sunasara@hotmail.com>
2024-11-10 15:05:19 +0530

    Expense Wizard with File Upload

---
+++ HisabPro.Web/Controllers/ImportController.cs
@@ -0,0 +1,90 @@
+﻿using HisabPro.DTO.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace HisabPro.Web.Controllers
+{
+    public class ImportController : Controller
+    {
+        public ActionResult Expense()
+        {
+            return View();
+        }
+
+        public ActionResult Income()
+        {
+            return View();
+        }
+
+        public IActionResult Upload()
+        {
+            return PartialView("_Upload");
+        }
+
+        [HttpGet]
+        public IActionResult Extraction(string filename)
+        {
+            return PartialView("_Extraction");
+        }
+
+        public IActionResult Validation()
+        {
+            return PartialView("_Validation");
+        }
+
+        public IActionResult Submit()
+        {
+            return PartialView("_Submit");
+        }
+
+        public async Task<IActionResult> SaveFile(IFormFile file)
+        {
+            var response = new ResponseDTO<object>();
+            response.StatusCode = HttpStatusCode.BadRequest;
+
+            if (file != null && file.Length > 0)
+            {
+                // Validate file extension
+                var extension = Path.GetExtension(file.FileName).ToLower();
+                if (extension != ".xls" && extension != ".xlsx")
+                {
+                    response.Message = "Only .xls and .xlsx files are allowed!";
+                }
+
+                // Extract original file name and extension
+                var originalFileName = Path.GetFileNameWithoutExtension(file.FileName);
+                var fileExtension = Path.GetExtension(file.FileName);
+                // Generate a unique name with a timestamp
+                var timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+                var newFileName = $"{originalFileName}-{timestamp}{fileExtension}";
+
+                try
+                {
+                    // Define the path to save the uploaded file
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", newFileName);
+
+                    // Save the file to the specified path
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+                response.StatusCode = HttpStatusCode.OK;
+                response.Message = "File uploaded successfully!";
+                response.Response = new { FileName = newFileName };
+            }
+            else
+            {
+                response.Message = "Please select a file to upload.";
+            }
+
+            return StatusCode((int)response.StatusCode, response);
+        }
+    }
+}
---
+++ HisabPro.Web/wwwroot/js/wizard.js
@@ -0,0 +1,87 @@
+﻿$(document).ready(function () {
+    let currentStep = 1;
+    var actions = ['Upload', 'Extraction', 'Validation', 'Submit'];
+    var wizardContent = '#wizard-content';
+    var wizardError = '#wizard-error';
+
+    function loadStep() {
+        if (currentStep == 2) {
+            hideError();
+            //Upload file is successful then load next step
+            var dataFile = $("#dataFile")[0];
+            var file = dataFile.files[0];
+
+            // Check if a file is selected
+            if (!file) {
+                showError('Please select a file to upload before proceeding to the next step.')
+            }
+            else {
+                // Create a FormData object to hold the file
+                var formData = new FormData();
+                formData.append("file", file);
+
+                $.ajax({
+                    url: '/import/savefile',
+                    type: 'POST',
+                    data: formData,
+                    contentType: false,  // Don't set content type
+                    processData: false,  // Don't process the data
+                    success: function (res) {
+                        const filename = res.response.fileName;
+                        const encodedFilename = encodeURIComponent(filename);
+                        const action = `${actions[currentStep - 1]}?filename=${encodedFilename}`;
+                        loadUI(action);
+                    },
+                    error: function (xhr, status, error) {
+                        var errorMessage = xhr.responseJSON ? xhr.responseJSON.message : "An error occurred during file upload.";
+                        showError(errorMessage);
+                    }
+                });
+            }
+        }
+        else {
+            loadUI(actions[currentStep - 1]);
+        }
+    }
+
+    function loadUI(action) {
+        $(wizardContent).load(`/Import/${action}`, function () {
+            updateStepUI(currentStep);
+        });
+    }
+
+    function updateStepUI() {
+        $(".step").removeClass("active complete");
+        $(".step").each(function () {
+            const stepNum = $(this).data("step");
+            if (stepNum < currentStep) $(this).addClass("complete");
+            else if (stepNum === currentStep) $(this).addClass("active");
+        });
+
+        // Set up next and previous button functionality
+        $(wizardContent).off("click", ".next").on("click", ".next", function () {
+            currentStep++;
+            loadStep();
+        });
+
+        $(wizardContent).off("click", ".prev").on("click", ".prev", function () {
+            currentStep--;
+            loadStep();
+        });
+    }
+
+    function showError(errorMessage) {
+        var error = $(wizardError);
+        if (!error.hasClass('visible')) {
+            error.addClass('visible');
+        }
+        error.text(errorMessage);
+    }
+
+    function hideError() {
+        $(wizardError).removeClass('visible');
+    }
+
+    // Initialize by loading the first step
+    loadStep();
+});
