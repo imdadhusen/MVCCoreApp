dcbb7d4cc49e09edac912a8f1c195701cdb1451a
Imdadhusen Sunasara <imdadhusen.sunasara@hotmail.com>
2024-12-21 21:09:45 +0530

    Handle delete refrence error

--- HisabPro.Web/Helper/CustomExceptionFilter.cs
+++ HisabPro.Web/Helper/CustomExceptionFilter.cs
@@ -1,7 +1,8 @@
 ﻿using HisabPro.Common;
+using HisabPro.Constants;
 using HisabPro.DTO.Model;
-using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using System.Net;
 
 namespace HisabPro.Web.Helper
@@ -17,7 +18,9 @@ namespace HisabPro.Web.Helper
 
         public void OnException(ExceptionContext context)
         {
-            if (context.Exception is CustomValidationException ex)
+            // Log the exception (optional)
+            var exception = context.Exception;
+            if (exception is CustomValidationException ex)
             {
                 _logger.LogError("CustomValidationException: {Message}", ex.Message);
                 context.Result = new JsonResult(new ResponseDTO<bool>(HttpStatusCode.BadRequest, ex.Message, false))
@@ -25,6 +28,22 @@ namespace HisabPro.Web.Helper
                     StatusCode = ex.StatusCode
                 };
                 context.ExceptionHandled = true;
+            }
+            else
+            {
+                string errorMessage = AppConst.ApiMessage.InternalError;
+                if (exception.InnerException != null && exception.InnerException.Message.ToUpper().Contains("DELETE"))
+                {
+                    errorMessage = AppConst.ApiMessage.ReferenceDeleteError;
+                }
+
+                // Create a generic response
+                var response = new ResponseDTO<bool>(HttpStatusCode.InternalServerError, errorMessage, false);
+                // Set the response status code and content type
+                context.HttpContext.Response.StatusCode = (int)response.StatusCode;
+                context.HttpContext.Response.ContentType = "application/json";
+                // Return the serialized response as JSON
+                context.Result = new JsonResult(response);
             }
         }
     }
